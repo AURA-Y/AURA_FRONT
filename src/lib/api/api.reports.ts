efcbb2fac79adbf46336465040cc79763eba48a4
신지웅 <gombang8047@gmail.com>
2026-01-02 12:01:40 +0900

    [신지웅] 방 생성 및 입장 토스트 삭제

--- src/lib/api/api.reports.ts
+++ src/lib/api/api.reports.ts
@@ -3,16 +3,8 @@ import { ReportMetadata } from "@/lib/types/reports.type";
 
 //지난 회의 목록을 보여주는 것 -> 간단한 데이터만 가져오는 코드(PostgreDB)
 
-export const getReportById = async (reportId: string) => {
-  return api.get<ReportMetadata>(`/reports/${reportId}`);
-};
-
 export const getReportsByIds = async (reportIds: string[]) => {
   if (!reportIds || reportIds.length === 0) return { data: [] };
   const idsParam = reportIds.join(",");
   return api.get<ReportMetadata[]>(`/reports/batch/by-ids?ids=${idsParam}`);
-};
-
-export const getAllReports = async () => {
-  return api.get<ReportMetadata[]>("/reports");
 };
