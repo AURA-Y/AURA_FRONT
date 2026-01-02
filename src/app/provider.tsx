e95c63ab038ce14f995dc799b5daa14301dbf783
신지웅 <gombang8047@gmail.com>
2026-01-02 19:54:38 +0900

    [신지웅] 패키지 설정 변경

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -3,7 +3,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
-
 import "@livekit/components-styles";
 import { Toaster } from "@/components/ui/sonner";
 
